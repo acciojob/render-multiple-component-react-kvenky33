c6e0d2f6794f24d4bd67eb5515fa5f395d1a018f
konalam venkatesh <konalamvenkatesh111@gmail.com>
2023-02-20 15:58:57 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">Youtube clone</h1>
+      <p data-ns-test="project-description">
+        Youtube app with multiple sections such as channels,playlist,vedios
+        etc...
+      </p>
+    </div>
+  );
+};
 
 export default App;
